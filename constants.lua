0890d67478e846c5d17a09d4d2daea0079f014c3
Sidoupiar <sidoupiar@qq.com>
2021-06-08 10:49:45 +0800

    优化代码

--- constants.lua
+++ constants.lua
@@ -4,7 +4,7 @@ local data =
 	settings =
 	{
 		remove = { "bool" , "startup" , false } ,
-		enable = { "bool" , "startup" , true , nil , nil , nil , nil , nil , nil , nil }
+		enable = { "bool" , "startup" , true , nil , nil , nil , nil , nil , nil }
 	} ,
 	
 	musicList = need( "MusicList" )
@@ -17,8 +17,8 @@ end
 data.musicList = musicList
 
 for index , musicData in pairs( data.musicList ) do
-	data.settings["music_"..index] = { "bool" , "startup" , true , nil , nil , nil , nil , nil , { "SISPBGM.music-name" , musicData[1] } , { "SISPBGM.music-description" , musicData[1] , musicData[2] or { "SISPBGM.music-nil" } } }
+	data.settings["music_"..index] = { "bool" , "startup" , true , nil , nil , nil , nil , { "SISPBGM.music-name" , musicData[1] } , { "SISPBGM.music-description" , musicData[1] , musicData[2] or { "SISPBGM.music-nil" } } }
 end
-data.settings.enable[10] = { "SISPBGM.music-enable-count" , #data.musicList-1 }
+data.settings.enable[9] = { "SISPBGM.music-enable-count" , #data.musicList-1 }
 
 return data
